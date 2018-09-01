19fb29aebe9de09aa482250dd3f82e8d6ac16370
hurtnotbad <1181942512@qq.com>
2018-09-01 20:09:15 +0800

    提交代码

---
+++ app/src/main/java/com/example/lammy/ffmpegdemo/FFmpegUtil.java
@@ -0,0 +1,21 @@
+package com.example.lammy.ffmpegdemo;
+
+/**
+ * Created by Lammy on 2018/9/1.
+ */
+
+public class FFmpegUtil {
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+        System.loadLibrary("avcodec-57");
+        System.loadLibrary("avfilter-6");
+        System.loadLibrary("avformat-57");
+        System.loadLibrary("avutil-55");
+        System.loadLibrary("swresample-2");
+        System.loadLibrary("swscale-4");
+    }
+
+    public native static void decode(String input,String output);
+
+}
---
+++ app/src/main/java/com/example/lammy/ffmpegdemo/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.lammy.ffmpegdemo;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+
+import static com.example.lammy.ffmpegdemo.FFmpegUtil.decode;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private int permissionRequestCode = 1;
+    private String inputFilePath = new File(Environment.getExternalStorageDirectory()+ "/a-lammytest/","test.mp4").getAbsolutePath();
+    private String outputFilePath = new File(Environment.getExternalStorageDirectory()+ "/a-lammytest/","output_yuv420p.yuv").getAbsolutePath();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        requestPermissions();
+
+    }
+
+    private String permissions[] = new String[]{
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+
+    };
+
+
+    private void requestPermissions(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            for(int i =0; i < permissions.length ; i ++) {
+
+                if( checkSelfPermission(permissions[0]) == PackageManager.PERMISSION_DENIED){
+                    requestPermissions(permissions ,permissionRequestCode );
+                    return;
+     //               Toast.makeText(this , "请打开"+permissions[0]+"权限",Toast.LENGTH_LONG).show();
+                }
+            }
+            decode(inputFilePath, outputFilePath);
+        }else{
+            decode(inputFilePath, outputFilePath);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if(requestCode ==permissionRequestCode ){
+
+            for(int result:grantResults){
+                if(result == PackageManager.PERMISSION_DENIED){
+                    return;
+                }
+            }
+
+            decode(inputFilePath, outputFilePath);
+
+        }
+    }
+}
